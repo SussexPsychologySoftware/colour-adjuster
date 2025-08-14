0d0056423e220b5802a906590d673a02d0b54571
Max-Lovell <max_lovell@hotmail.co.uk>
2025-08-14 15:07:30 +0100

    New colour adjuster screen

--- app/adjust.tsx
+++ app/adjust.tsx
@@ -1,136 +1,87 @@
 import {Text, View, StyleSheet, Pressable} from "react-native";
 import {useEffect, useState} from "react";
 import AdjustColourButton from '@/components/AdjustColourButton'
-import { ColourUtils } from '@/utils/colourConversion';
+import { ColourConverter } from '@/utils/colourConversion';
 import { colourConstraints } from '@/constants/colourConstraints';
+import WhiteTrial from '@/components/WhiteTrial'
+import { ABAxis, LAB, RGB, LCH, Constraint, TargetColour, Range } from "@/types/colours";
 
-type abAxis = 'blue' | 'red' | 'yellow' | 'green';
-type lab = {l:number, a:number, b:number}
-// In future this will call 'Hue trial' or 'Normal trial' components
+type trialType = 'white'|'hue'
+
 export default function AdjustColourScreen() {
 
-    const [backgroundColour, setBackgroundColour] = useState({r: 50, g: 50, b: 50});
-    const [aUpperBoundReached, setAUpperBoundReached] = useState(false);
-    const [aLowerBoundReached, setALowerBoundReached] = useState(false);
-    const [bUpperBoundReached, setBUpperBoundReached] = useState(false);
-    const [bLowerBoundReached, setBLowerBoundReached] = useState(false);
+    // Maybe background should be set here, and then the trial components change it as a prop?
+        // but probably don't want them to rerender when it changes?
+    const [backgroundColour, setBackgroundColour] = useState<RGB>({r: 50, g: 50, b: 50});
     const [submitting, setSubmitting] = useState(false);
+    const [trialType, setTrialType] = useState<TargetColour|null>(null); // or just index on trial number??
+    const [trialNumber, setTrialNumber] = useState(null);
+    const [trials, setTrials] = useState<TargetColour[]>([]);
+    const [targetColour, setTargetColour] = useState('white');
 
-    useEffect(() => {
 
-    }, [backgroundColour])
+    function startTrials(){
 
-
-    function getNewLAB(button: abAxis){
-        // convert to lab
-        const lab = ColourUtils.rgb2lab(backgroundColour)
-        //change A or B according to button pressed
-        if(button ==='red') lab.a++
-        else if(button ==='blue') lab.b--
-        else if(button ==='yellow') lab.b++
-        else if(button ==='green') lab.a--
-        return lab
     }
 
-    function testABChange(lab: lab, axisKey: 'a'|'b', change: 1|-1){
-        const predictedLAB = {...lab} // TODO: these shouldn't be changing in place really?
-        // Get predicted value after change in LAB and LCH
-        predictedLAB[axisKey] += change // Change relevant value
-        const predictedLCH = ColourUtils.lab2lch(predictedLAB) // Convert to lch
-        // Compare to constraints
-        // to check abBounds = predictedLAB[axisKey] < -128 || predictedLAB[axisKey] > 127
-        return predictedLCH.c < 0 || predictedLCH.c > colourConstraints.White.c
+    // Function to shuffle the array (Fisher-Yates Shuffle)
+    function shuffle(array: string[]) {
+        for(let i=array.length-1; i>0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];  // Swap
+        }
     }
 
-    function checkToggleButtons(lab: lab){
-        // Disable if a +/- 1 change in relevant a or b would push chroma out of bounds
-        const up = testABChange(lab,'a',1)
-        setAUpperBoundReached(up)
-        const down = testABChange(lab,'a',-1)
-        setALowerBoundReached(down)
-        const left = testABChange(lab,'b',-1)
-        setBLowerBoundReached(left)
-        const right = testABChange(lab,'b',1)
-        setBUpperBoundReached(right)
+    // Creating trials array
+    function createTrialsArray(): string[]{
+        const trials: TargetColour[] = []
+        const hues: TargetColour[] = ['red', 'green', 'blue', 'yellow']
+        const nBlocks = 3
+        for (let b=0; b<nBlocks; b++) {
+            shuffle(hues) // Random shuffle on hues
+            for(let h=0; h<hues.length; h++){
+                trials.push('white')
+                trials.push(hues[h])
+            }
+        }
+        return trials
     }
 
-    const handlePress = (button: abAxis) => {
-        const lab = getNewLAB(button)
-        const rgb = ColourUtils.lab2rgb(lab)
-        setBackgroundColour(rgb)
-        checkToggleButtons(lab)
+    function startNewTrial(){
+        // Increase trial index
+        const nextTargetColour = 'white'
+        setTargetColour(nextTargetColour)
+        const randomColour = randomStartingColour(nextTargetColour)
+        setBackgroundColour(randomColour)
+    }
+
+    function getRandomHue(hueRanges: Range[]) {
+        // Select a random range from hue ranges
+        const randomRange = hueRanges[Math.floor(Math.random() * hueRanges.length)];
+        // Generate a random hue within the selected range
+        return Math.floor(Math.random() * (randomRange.max - randomRange.min+1) + randomRange.min);
+    }
+
+    function randomStartingColour(targetColour: TargetColour) {
+        const constraints = colourConstraints[targetColour]
+        const randomHue = getRandomHue(constraints.hueRanges) // random hue within the allowed ranges
+        return ColourConverter.lch2rgb({ ...constraints, h: randomHue })
     }
 
     const handleSubmit = () => {
         setSubmitting(true)
+        try {
+            // Save data
+        } catch (e) {
+            console.log(e)
+        } finally {
+            setSubmitting(false)
+        }
         // Save data, reset buttons and colour and restart
     }
 
     return (
-        <View style={[styles.container, {backgroundColor: `rgb(${backgroundColour.r}, ${backgroundColour.g}, ${backgroundColour.b})` }]}>
-            <AdjustColourButton disabled={aUpperBoundReached} onPress={()=>handlePress('red')} style={styles.top}/>
-            <View style={styles.middle}>
-                <AdjustColourButton disabled={bLowerBoundReached} onPress={()=>handlePress('blue')} style={styles.left}/>
-                <View style={styles.infoAndSubmit}>
-                    <Text style={[styles.text, styles.targetColour]}>White</Text>
-                    <Pressable disabled={submitting} onPress={handleSubmit} style={[styles.submitButton, {borderColor: submitting ? 'grey' : 'black'}]}>
-                        <Text style={[styles.text, styles.submitText, {color: submitting ? 'grey' : 'black'}]}>Submit</Text>
-                    </Pressable>
-                </View>
-                <AdjustColourButton disabled={bUpperBoundReached} onPress={()=>handlePress('yellow')} style={styles.right}/>
-            </View>
-            <AdjustColourButton disabled={aLowerBoundReached} onPress={()=>handlePress('green')} style={styles.bottom}/>
-        </View>
+        <WhiteTrial targetColour={targetColour} startColour={{r: 50, g: 50, b: 50}} onSubmit={handleSubmit}/>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        height: "100%",
-        width: "100%",
-        maxHeight: "100%",
-        maxWidth: "100%",
-        backgroundColor: "black",
-        justifyContent: "space-between"
-    },
-    top: {
-        alignSelf: "center",
-    },
-    bottom: {
-        alignSelf: "center",
-        justifyContent: "flex-end",
-    },
-    middle: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center"
-    },
-    left: {
-    },
-    right: {
-        // alignSelf: "flex-end",
-    },
-
-    infoAndSubmit: {
-        alignItems: "center"
-    },
-    submitButton: {
-        paddingVertical: 12,
-        paddingHorizontal: 20,
-        margin: 10,
-        borderWidth: 1,
-        borderRadius: 10,
-    },
-    text: {
-        fontWeight: "bold",
-        color: "black",
-    },
-    targetColour: {
-        fontWeight: "bold",
-        fontSize: 30,
-    },
-    submitText: {
-        fontSize: 15,
-    }
-});
 
