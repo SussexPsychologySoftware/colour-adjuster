aaf955099b4acb87c0f85f61f54ea9b1887d61e9
Max Lovell <max_lovell@hotmail.co.uk>
2025-08-15 14:39:12 +0100

    Create file for abstracted component grouping White and Hue Trial components

---
+++ components/Trial.tsx
@@ -0,0 +1,7 @@
+import { ABAxis, LAB, RGB, LCH, Constraint, TargetColour, Range } from "@/types/colours";
+
+export default function Trial({ startColour, targetColour, onSubmit }: {startColour: RGB, targetColour: string, onSubmit: ()=>void}) {
+    // For abstraction of White and Hue trials generally.
+    return (<></>)
+}
+
