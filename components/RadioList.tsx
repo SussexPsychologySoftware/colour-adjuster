e0fd7f03a785cb733a3e5fc6becf335a785683d7
Max Lovell <max_lovell@hotmail.co.uk>
2025-08-16 15:15:06 +0100

    Add radio button list

---
+++ components/RadioList.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons';
+
+
+export default function RadioList({ options, onSelect} : { options: string[], onSelect: (option: string)=>void }) {
+    const [selectedOption, setSelectedOption] = useState('');
+    const onOptionPress = (option: string) => {
+        setSelectedOption(option);
+        onSelect(option);
+    }
+    return(
+        <>
+            {options.map((option, index) => (
+                <TouchableOpacity key={option} style={styles.container} onPress={()=>onOptionPress(option)}>
+                    <MaterialIcons
+                        name={selectedOption===option ? 'radio-button-on' : 'radio-button-off'}
+                        size={15}
+                        color="black"
+                    />
+                    <Text style={styles.text}>{option}</Text>
+                </TouchableOpacity>
+            ))}
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10
+    },
+    text: {
+        fontSize: 15,
+    }
+})
+
+
+
