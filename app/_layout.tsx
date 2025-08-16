8187adb8e8f47cbb33bf90945ddce05bec83bfdf
Max Lovell <max_lovell@hotmail.co.uk>
2025-08-16 17:56:43 +0100

    Add use restore to layout page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { Stack } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
 import {View} from "react-native";
+import {useRestore} from "@/hooks/useRestore";
 
 export default function RootLayout() {
-  return (
+    useRestore() // go to testing page if finished
+
+    return (
       <View style={{ flex: 1, backgroundColor: 'black' }}>
           <StatusBar hidden translucent/>
           <Stack screenOptions={{
