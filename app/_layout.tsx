270006b8882c31c5ddb6d6ff4a09432750c0da15
Max Lovell <max_lovell@hotmail.co.uk>
2025-08-16 17:08:58 +0100

    Add new pages to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,24 @@
 import { Stack } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
+import {View} from "react-native";
 
 export default function RootLayout() {
-  return (<>
-    <StatusBar hidden translucent/>
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index"/>
-      <Stack.Screen name="adjust"/>
-      <Stack.Screen name="survey"/>
-    </Stack>
-  </>
+  return (
+      <View style={{ flex: 1, backgroundColor: 'black' }}>
+          <StatusBar hidden translucent/>
+          <Stack screenOptions={{
+            // Just incase
+            headerShown: false,
+            headerStyle: { backgroundColor: 'black' },
+            headerTintColor: 'black',
+            contentStyle: { backgroundColor: 'black' }
+          }}>
+              <Stack.Screen name="index"/>
+              <Stack.Screen name="consent"/>
+              <Stack.Screen name="confirmSettings"/>
+              <Stack.Screen name="adjust"/>
+              <Stack.Screen name="survey"/>
+          </Stack>
+    </View>
   )
 }
