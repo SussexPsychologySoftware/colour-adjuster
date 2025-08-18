2b3ea361bf26d7e0b343288672cb2455b49f5a8b
Max Lovell <max_lovell@hotmail.co.uk>
2025-08-18 13:39:50 +0100

    Allow click & hold for adjust colour buttons, feedback on press

--- components/AdjustColourButton.tsx
+++ components/AdjustColourButton.tsx
@@ -1,14 +1,57 @@
-import { Text, StyleSheet, Pressable } from 'react-native';
+import { Text, StyleSheet, Pressable, View } from 'react-native';
+import { useState, useRef, useEffect } from "react";
 
 export default function AdjustColourButton({ style, disabled, onPress }: {style: object, disabled: boolean, onPress: ()=>void}) {
 
-    const disabledColour = 'grey'
+    const [pressed, setPressed] = useState(false);
+    const intervalRef = useRef<number | null>(null);
+
+    const endPress = () => {
+        setPressed(false)
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+    };
+
+    const startLongPress = () => {
+        if(disabled || intervalRef.current) return; // If disabled or already interval running
+        setPressed(true)
+        intervalRef.current = setInterval(onPress, 50);
+    };
+
+    useEffect(endPress, [disabled]);
+
+    // Cleanup on unmount just incase
+    useEffect(() => {
+        return () => {
+            endPress();
+        };
+    }, []);
+
+    const disabledColour = 'grey';
+
     return (
-        <Pressable disabled={disabled} onPress={onPress} onLongPress={onPress}
-                   style={[styles.adjustColourButton, style, {borderColor: disabled ? disabledColour : 'black'}]}>
-            <Text style={[styles.text,{color: disabled ? disabledColour : 'black'}]}>+</Text>
+        <Pressable
+            disabled={disabled}
+            onPress={onPress}
+            onPressIn={()=>setPressed(true)}
+            onLongPress={startLongPress}
+            onPressOut={endPress}
+            delayLongPress={200}
+        >
+            <View style={[
+                    styles.adjustColourButton,
+                    style,
+                    {
+                        borderColor: disabled ? disabledColour : 'black',
+                        opacity: pressed ? 0.7 : 1
+                    }
+                ]}>
+                <Text style={[styles.text, {color: disabled ? disabledColour : 'black'}]}>+</Text>
+            </View>
         </Pressable>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -17,12 +60,9 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         margin: 10,
         borderWidth: 1,
-        // borderColor: 'black',
         borderRadius: 20,
     },
     text: {
-        // color: 'black',
         fontSize: 30,
-        // fontWeight: 300,
     }
 });
