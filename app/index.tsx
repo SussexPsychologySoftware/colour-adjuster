e40421d0e72d2fa84e81bcad11e3bbd3f0c0ce93
Max Lovell <max_lovell@hotmail.co.uk>
2025-08-18 20:40:23 +0100

    Remove unused components from index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,2 @@
-import { Text, View } from "react-native";
-import {Link, RelativePathString, router, Stack} from "expo-router";
-import {useEffect} from "react";
 export { default } from './consent';
 
