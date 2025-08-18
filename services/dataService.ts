518200c9d7094d16f59408c1b0b83935bb39bd17
Max Lovell <max_lovell@hotmail.co.uk>
2025-08-18 18:27:42 +0100

    Add data service class

---
+++ services/dataService.ts
@@ -0,0 +1,62 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { dataQueue } from './dataQueue';
+
+export class DataService {
+    static async getData(key: string) {
+        const dataString = await AsyncStorage.getItem(key);
+        return dataString ? JSON.parse(dataString) : null;
+    }
+
+    static async saveData(data: Record<string, any>, name: string, datapipeId?: string) {
+        data.timestamp = new Date().toISOString();
+        const participantId = await this.getParticipantID();
+        if (!participantId) throw new Error("Participant ID not found");
+        data.participantId = participantId;
+
+        const dataString = JSON.stringify(data);
+        await AsyncStorage.setItem(name, dataString);
+        // data: CdE5fn8ckU5w, participants: eXM0k3gPdL9y
+        if (datapipeId) {
+            const filename = `${participantId}_${name}`
+            try {
+                const response = await this.sendToServer(dataString, filename, datapipeId);
+                // Only should have 1 for each day so could totally just tag by day?
+                if(!response.ok) await dataQueue.addToQueue(dataString, filename, datapipeId);
+            } catch (error) {
+                // Network error, server unreachable, etc.
+                console.error('Error sending to datapipe: ', error);
+                await dataQueue.addToQueue(dataString, filename, datapipeId);
+            }
+        }
+    }
+
+    static async sendToServer(data: string, filename: string, experimentID: string) {
+        return await fetch("https://pipe.jspsych.org/api/data/", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "*/*",
+            },
+            body: JSON.stringify({
+                experimentID: experimentID,
+                filename: `${filename}.json`,
+                data: data,
+            }),
+        });
+    }
+
+    static async deleteData(id: string) {
+        await AsyncStorage.removeItem(id);
+    }
+
+    // ============ Participant ID ============
+    // Bit easier to just set this as a single string directly, used quite often
+    static async setParticipantID(id: string) {
+        await AsyncStorage.setItem('participantID', id);
+    }
+
+    static async getParticipantID() {
+        return await AsyncStorage.getItem('participantID');
+    }
+
+}
