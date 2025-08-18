7e7cb245be473fb6ec4f1061d51d8e55894e659c
Max Lovell <max_lovell@hotmail.co.uk>
2025-08-18 20:08:25 +0100

    Add submit button component

---
+++ components/SubmitButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import {Text, StyleSheet, Pressable} from 'react-native'
+
+export default function SubmitButton({ text, disabledText, disabled, onPress, style, textStyle } : { text: string, disabledText?: string, disabled: boolean, onPress: () => void, style?: object, textStyle?: object }) {
+    return(
+        <Pressable disabled={disabled} onPress={onPress} style={[styles.button, disabled && styles.disabled, style]}>
+            <Text style={[styles.text, textStyle, disabled && styles.disabled]}>{ disabled ? (disabledText??text) : text}</Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        alignSelf: 'center',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderWidth: 1,
+        borderColor: 'grey',
+        borderRadius: 10,
+        marginVertical: 10
+    },
+    disabled: {
+        // backgroundColor: 'gray',
+        opacity: 0.5
+    },
+    text: {
+        fontSize: 15,
+        color: 'lightgrey',
+        justifyContent: 'center',
+        fontWeight: "bold",
+    }
+})
+
