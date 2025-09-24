d82d99e6081e155a8dfe192b64b5aa036639c8c8
Max Lovell <max_lovell@hotmail.co.uk>
2025-09-24 12:57:37 +0100

    Increase submit button margin to keep on screen

--- components/SubmitButton.tsx
+++ components/SubmitButton.tsx
@@ -18,7 +18,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'grey',
         borderRadius: 10,
-        marginVertical: 15
+        marginVertical: 20
     },
     disabled: {
         // backgroundColor: 'gray',
