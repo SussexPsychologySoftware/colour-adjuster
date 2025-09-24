fa3b6daff978f771b23e589885885d1b943ac5c1
Max Lovell <max_lovell@hotmail.co.uk>
2025-09-24 10:54:18 +0100

    Add keyboard provider wrapper to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,28 +2,31 @@ import { Stack } from "expo-router";
 import { StatusBar } from 'expo-status-bar';
 import {View} from "react-native";
 import {useRestore} from "@/hooks/useRestore";
+import {KeyboardProvider} from "react-native-keyboard-controller";
 
 export default function RootLayout() {
     useRestore() // go to testing page if finished
 
     return (
-      <View style={{ flex: 1, backgroundColor: 'black' }}>
-          <StatusBar hidden translucent/>
-          <Stack screenOptions={{
-            // Just incase
-            headerShown: false,
-            headerStyle: { backgroundColor: 'black' },
-            headerTintColor: 'black',
-            contentStyle: { backgroundColor: 'black' }
-          }}>
-              <Stack.Screen name="index"/>
-              <Stack.Screen name="consent"/>
-              <Stack.Screen name="confirmSettings"/>
-              <Stack.Screen name="adjust"/>
-              <Stack.Screen name="survey"/>
-              <Stack.Screen name="debrief"/>
-              <Stack.Screen name="testing"/>
-          </Stack>
-    </View>
+        <KeyboardProvider>
+            <View style={{ flex: 1, backgroundColor: 'black' }}>
+              <StatusBar hidden translucent/>
+              <Stack screenOptions={{
+                // Just incase
+                headerShown: false,
+                headerStyle: { backgroundColor: 'black' },
+                headerTintColor: 'black',
+                contentStyle: { backgroundColor: 'black' }
+              }}>
+                  <Stack.Screen name="index"/>
+                  <Stack.Screen name="consent"/>
+                  <Stack.Screen name="confirmSettings"/>
+                  <Stack.Screen name="adjust"/>
+                  <Stack.Screen name="survey"/>
+                  <Stack.Screen name="debrief"/>
+                  <Stack.Screen name="testing"/>
+              </Stack>
+        </View>
+    </KeyboardProvider>
   )
 }
